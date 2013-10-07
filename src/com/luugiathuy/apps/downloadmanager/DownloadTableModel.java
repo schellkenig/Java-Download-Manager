a1665bede5e3242b15b2dea3387179bd50c0437b
Christopher Mueller <christopher.mueller@bitmovin.net>
2013-10-07 16:05:52 +0200

    Fixed style

--- src/com/luugiathuy/apps/downloadmanager/DownloadTableModel.java
+++ src/com/luugiathuy/apps/downloadmanager/DownloadTableModel.java
@@ -21,7 +21,7 @@ HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
 WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.
-*/
+ */
 
 package com.luugiathuy.apps.downloadmanager;
 
@@ -32,98 +32,105 @@ import javax.swing.JProgressBar;
 import javax.swing.table.AbstractTableModel;
 
 /**
- *  This class manages the download table's data.
- *
+ * This class manages the download table's data.
+ * 
  */
-public class DownloadTableModel extends AbstractTableModel implements Observer {
-    
-	private static final long serialVersionUID = -7852567715605820609L;
+public class DownloadTableModel extends AbstractTableModel implements Observer
+{
 
-	// These are the names for the table's columns.
-    private static final String[] columnNames = {"URL", "Size (KB)",
-    "Progress", "Status"};
-    
+    private static final long serialVersionUID = -7852567715605820609L;
+
+    // These are the names for the table's columns.
+    private static final String[] columnNames = { "URL", "Size (KB)", "Progress", "Status" };
+
     // These are the classes for each column's values.
     @SuppressWarnings("rawtypes")
-	private static final Class[] columnClasses = {String.class,
-    	String.class, JProgressBar.class, String.class};
-    
+    private static final Class[] columnClasses = { String.class, String.class, JProgressBar.class, String.class };
+
     /**
-     *  Add a new download to the table.
+     * Add a new download to the table.
      */
-    public void addNewDownload(Downloader download) {
+    public void addNewDownload(Downloader download)
+    {
         // Register to be notified when the download changes.
         download.addObserver(this);
-        
+
         // Fire table row insertion notification to table.
         fireTableRowsInserted(getRowCount() - 1, getRowCount() - 1);
     }
-    
-    
+
     /**
-     *  Remove a download from the list.
+     * Remove a download from the list.
      */
-    public void clearDownload(int row) {        
+    public void clearDownload(int row)
+    {
         // Fire table row deletion notification to table.
         fireTableRowsDeleted(row, row);
     }
-    
+
     /**
-     *  Get table's column count.
+     * Get table's column count.
      */
-    public int getColumnCount() {
+    public int getColumnCount()
+    {
         return columnNames.length;
     }
-    
+
     /**
-     *  Get a column's name.
+     * Get a column's name.
      */
-    public String getColumnName(int col) {
+    public String getColumnName(int col)
+    {
         return columnNames[col];
     }
-    
+
     /**
-     *  Get a column's class.
+     * Get a column's class.
      */
     @SuppressWarnings({ "rawtypes", "unchecked" })
-	public Class getColumnClass(int col) {
+    public Class getColumnClass(int col)
+    {
         return columnClasses[col];
     }
-    
+
     /**
-     *  Get table's row count.
+     * Get table's row count.
      */
-    public int getRowCount() {
+    public int getRowCount()
+    {
         return DownloadManager.getInstance().getDownloadList().size();
     }
-    
+
     /**
-     *  Get value for a specific row and column combination.
+     * Get value for a specific row and column combination.
      */
-    public Object getValueAt(int row, int col) {
-    	// Get download from download list
+    public Object getValueAt(int row, int col)
+    {
+        // Get download from download list
         Downloader download = DownloadManager.getInstance().getDownloadList().get(row);
-        
-        switch (col) {
-            case 0: // URL
-                return download.getURL();
-            case 1: // Size
-                int size = download.getFileSize();
-                return (size == -1) ? "" : (Integer.toString(size/1000));
-            case 2: // Progress
-                return new Float(download.getProgress());
-            case 3: // Status
-                return Downloader.STATUSES[download.getState()];
+
+        switch (col)
+        {
+        case 0: // URL
+            return download.getURL();
+        case 1: // Size
+            int size = download.getFileSize();
+            return (size == -1) ? "" : (Integer.toString(size / 1000));
+        case 2: // Progress
+            return new Float(download.getProgress());
+        case 3: // Status
+            return Downloader.STATUSES[download.getState()];
         }
         return "";
     }
-    
+
     /**
      * Update is called when a Download notifies its observers of any changes
      */
-    public void update(Observable o, Object arg) {
+    public void update(Observable o, Object arg)
+    {
         int index = DownloadManager.getInstance().getDownloadList().indexOf(o);
-        
+
         // Fire table row update notification to table.
         fireTableRowsUpdated(index, index);
     }
--- src/com/luugiathuy/apps/downloadmanager/ProgressRenderer.java
+++ src/com/luugiathuy/apps/downloadmanager/ProgressRenderer.java
@@ -21,7 +21,7 @@ HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
 WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.
-*/
+ */
 
 package com.luugiathuy.apps.downloadmanager;
 
@@ -32,26 +32,28 @@ import javax.swing.JTable;
 import javax.swing.table.TableCellRenderer;
 
 /**
- *  This class renders a JProgressBar in a table cell.
+ * This class renders a JProgressBar in a table cell.
  */
-public class ProgressRenderer extends JProgressBar implements TableCellRenderer {
-    
-	private static final long serialVersionUID = -2002374113358949051L;
+public class ProgressRenderer extends JProgressBar implements TableCellRenderer
+{
 
-	/**
-	 *  Constructor for ProgressRenderer.
-	 */
-    public ProgressRenderer(int min, int max) {
+    private static final long serialVersionUID = -2002374113358949051L;
+
+    /**
+     * Constructor for ProgressRenderer.
+     */
+    public ProgressRenderer(int min, int max)
+    {
         super(min, max);
     }
-    
+
     /**
-     * Returns this JProgressBar as the renderer for the given table cell. 
+     * Returns this JProgressBar as the renderer for the given table cell.
      */
-    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
-            											boolean hasFocus, int row, int column) {
-        
-    	// set JProgressBar's percent complete value.
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column)
+    {
+
+        // set JProgressBar's percent complete value.
         setValue((int) ((Float) value).floatValue());
         return this;
     }
